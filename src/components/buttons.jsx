4425f090cb46aa56f9396c0f1a0424c5f8251b7e
ADAidan <ADyer2190@gmail.com>
2024-02-19 05:05:06 -0500

    implemented fontawesome icons into resign and offer draw button

--- src/components/buttons.jsx
+++ src/components/buttons.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import './buttons.css';
+import { faFlag, faHandshake } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const ResignButton = () => {
     return (
-        <button className='resign-button'>Resign</button>
+        <button className='resign-button'>
+            <FontAwesomeIcon icon={faFlag}/> 
+            Resign
+        </button>
     );
 };
 
 export const DrawButton = () => {
     return (
-        <button className='draw-button'>Offer Draw</button>
+        <button className='draw-button'>
+            <FontAwesomeIcon icon={faHandshake}/> 
+            Offer Draw
+        </button>
     );
 };
