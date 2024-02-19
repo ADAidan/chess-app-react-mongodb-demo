82072c56bda9702ca2d261deefe99c21769ead71
ADAidan <ADyer2190@gmail.com>
2024-02-19 16:54:35 -0500

    implemented auto scrolling for the move history component

--- src/components/move-history.jsx
+++ src/components/move-history.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import './move-history.css';
 
 const MoveHistory = ({history}) => {
+    const moveHistoryRef = useRef(null);
+
+    useEffect(() => {
+        moveHistoryRef.current.scrollTop = moveHistoryRef.current.scrollHeight;
+    }, [history]);
+
     return (
-        <div className='move-history-container'>
+        <div className='move-history-container' 
+        ref={moveHistoryRef}>
             <ol>
                 {history.map((move, index) => {
                     if (index % 2 === 0) {
