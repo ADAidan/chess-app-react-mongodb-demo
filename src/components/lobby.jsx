183bd10ce93b2956d78243e7955cfd49b5a58d19
ADAidan <ADyer2190@gmail.com>
2024-03-01 20:24:04 -0500

    created a container for the games in the lobby

--- src/components/lobby.jsx
+++ src/components/lobby.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import './lobby.css';
 
 const Lobby = () => {
     return (
         <>
-            <div>Lobby</div>
+            <div className='lobby-container'>
+            </div>
         </>
     )
 };
