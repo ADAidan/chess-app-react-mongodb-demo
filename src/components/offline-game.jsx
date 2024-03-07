225f5e44c16a19ce6648f5697dc848577b7bb8cc
ADAidan <ADyer2190@gmail.com>
2024-03-06 22:32:20 -0500

    added offline component

---
+++ src/components/offline-game.jsx
@@ -0,0 +1,12 @@
+import './offline-game.css';
+
+const OfflineGame = () => {
+    return (
+        <div className='offline-game-container'>
+            <h1>Offline Game</h1>
+            <p>Play a game of chess against the computer</p>
+        </div>
+    );
+};
+
+export default OfflineGame;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import ErrorPage from './error-page.jsx'
 import { 
   createBrowserRouter, 
   RouterProvider 
 } from 'react-router-dom'
+import App from './App.jsx'
+import ErrorPage from './error-page.jsx'
 import ChessGame from './components/game.jsx'
 import Lobby from './components/lobby.jsx'
 import CreateGame from './components/create-game.jsx'
+import OfflineGame from './components/offline-game.jsx'
 import './index.css'
 
 const router = createBrowserRouter([
@@ -28,7 +29,11 @@ const router = createBrowserRouter([
   {
     path: '/create-game',
     element: <CreateGame />,
-  }
+  },
+  {
+    path: '/offline',
+    element: <OfflineGame />,
+  },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
