8b9eaec54fbaa6a9741fe2fdb61f799c3003bb0a
ADAidan <ADyer2190@gmail.com>
2024-03-09 01:00:52 -0500

    added timer function

--- src/components/create-game.jsx
+++ src/components/create-game.jsx
@@ -4,12 +4,38 @@ import './create-game.css';
 
 const CreateGame = () => {
 
+  const timer = (minutes, seconds) => {
+    switch (true) {
+      case minutes === '':
+        minutes = 0;
+        break;
+      case seconds === '':
+        seconds = 0;
+        break;
+      case minutes.length > 2:
+        return 'error: minutes too long';
+      case seconds.length > 2:
+        return 'error: seconds too long';
+      case seconds >= 60:
+        return 'error: seconds must be less than 60';
+      case String(seconds).length <= 1:
+        seconds = `0${String(seconds)}`;
+        break;
+      default:
+        break;
+    }
+    return `${minutes}:${seconds}`
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    const gameName = e.target[0].value;
-    const timeLimit = e.target[1].value;
-    const increment = e.target[2].value;
-    console.log(gameName, timeLimit, increment);
+    const username = sessionStorage.getItem('username');
+    const gameName = `${username}'s game`;
+    const timeLimitMinutes = e.target[0].value || 10;
+    const timeLimitSeconds = e.target[1].value || 0;
+    const increment = e.target[2].value || 0;
+    const timeLimit = timer(timeLimitMinutes, timeLimitSeconds);
+    console.log(gameName, timeLimit, `${increment}s`);
   };
 
   return (
